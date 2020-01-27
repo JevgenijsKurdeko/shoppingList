c84419a25da5b7c3504299594448c103dfd8693a
Jevgenijs Kurdeko <garrilase@inbox.lv>
2020-01-27 23:52:31 +0200

    1. Add three new fields in product: discount, category, description. 2. Add validations of name, price and discount.

--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -19,22 +19,53 @@ class ShoppingListApplication {
                 Integer userInput = Integer.valueOf(scanner.nextLine());
                 switch (userInput) {
                     case 1:
-                        System.out.println("Enter product name: ");
-                        String name = scanner.nextLine();
-                        System.out.println("Enter product price: ");
-                        BigDecimal price = new BigDecimal(scanner.nextLine());
+                        String name;
+                        do {
+                            System.out.println("Enter product name: (More than 3 and less than 32)");
+                            name = scanner.nextLine();
+                        }
+                        while (name.length() < 3 || name.length() > 32);
+
+                        double price;
+                        do {
+                            System.out.println("Enter product price:  (Product price must be greater than 0)");
+                            price = scanner.nextDouble();
+                            //BigDecimal price = new BigDecimal(scanner.nextLine());
+                        }
+                        while (price < 1);
+                        scanner.nextLine();
+
+                        System.out.println("Enter category: ");
+                        String category = scanner.nextLine();
+
+                        double discount;
+                        do {
+                            System.out.println("Enter discount: (Max 100%)");
+                            discount = scanner.nextDouble();
+                        }
+                        while (discount < 1 || discount > 100);
+                        scanner.nextLine();
+
+                        System.out.println("Enter description: ");
+                        String description = scanner.nextLine();
+
                         Product product = new Product();
                         product.setName(name);
                         product.setPrice(price);
                         product.setId(productIdSequence);
+                        product.setCategory(category);
+                        product.setDescription(description);
+                        product.setDiscount(discount);
                         productRepository.put(productIdSequence, product);
                         productIdSequence++;
-                        System.out.println("Result: " + product.getId());
+                        System.out.println("Result: " + product.getId() + product.getName() + product.getPrice() + product.getDiscount() + product.getCategory() + product.getDescription());
+                        break;
                     case 2:
                         System.out.println("Enter product id: ");
                         long id = scanner.nextLong();
                         Product findProductResult = productRepository.get(id);
                         System.out.println(findProductResult);
+                        break;
                     case 3:
                         return;
                 }
