fcf1897972a2ab2406d0ef9256d7bc97585ad69c
Jevgenijs Kurdeko <garrilase@inbox.lv>
2020-01-28 00:58:52 +0200

    1. Add three new fields in product: discount, category, description. 2. Add validations of name, price and discount.

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -6,7 +6,11 @@ public class Product {
 
     private Long id;
     private String name;
-    private BigDecimal price;
+    //private BigDecimal price;
+    private double price;
+    private String category;
+    private double discount;
+    private String description;
 
     public Long getId() {
         return id;
@@ -24,12 +28,44 @@ public class Product {
         this.name = name;
     }
 
-    public BigDecimal getPrice() {
+//    public BigDecimal getPrice() {
+//      return price;
+//    }
+//
+//    public void setPrice(BigDecimal price) {
+//        this.price = price;
+//    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
+    public void setPrice(double price) {
         this.price = price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
 }
